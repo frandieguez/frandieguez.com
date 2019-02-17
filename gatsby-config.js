341871ed0e93e738b57501b1a294cb8ce9846d43
Fran Dieguez <fran.dieguez@mabishu.com>
2019-02-17 12:53:06 +0100

    Add base gastby installation without contents

---
+++ gatsby-config.js
@@ -0,0 +1,78 @@
+module.exports = {
+  siteMetadata: {
+    title: `Fran Dieguez | Yet another software craftmanship`,
+    author: `Fran Dieguez`,
+    description: ``,
+    siteUrl: `https://www.frandieguez.com/`,
+    social: {
+      twitter: `frandieguez`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Fran Dieguez | Yet another software craftmanship`,
+        short_name: `Fran Dieguez`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#00aeef`,
+        display: `minimal-ui`,
+        icon: `content/assets/logos/glasses.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
