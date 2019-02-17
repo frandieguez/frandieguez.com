7af78226919443d9ffd647d8c4a7a8fcfa53757c
Fran Dieguez <fran.dieguez@mabishu.com>
2019-02-17 17:55:57 +0100

    Fix living location in bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -35,8 +35,7 @@ function Bio() {
             <p style={{
               marginTop: 25
             }}>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong> who lives and works in Spain building useful things.
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
                 You should follow him on Twitter
