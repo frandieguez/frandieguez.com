49e842ff20805f12d12fca2dd7abd4b3725f2955
Fran Dieguez <fran.dieguez@mabishu.com>
2019-02-22 19:16:11 +0100

    Create a new about page

---
+++ src/pages/about.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+class AboutPage extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="About" />
+
+        <img style="float: left; margin:" alt="859795_10200638722191374_162643047_o" src="http://www.mabishu.com/wp-content/uploads/2009/10/859795_10200638722191374_162643047_o-300x300.jpg" width="300" height="300" />
+
+        <strong>Fran Diéguez</strong> has been developing web applications for 7 years now, the last two as a <strong>Project Manager and CTO at OpenHost</strong>, where he spends his days and nights on improving a powerful tool for online publishing, <a href="http://www.opennemas.com/">OpenNemas</a>.  For achieving this hard task he and his team are developing their own framework, Onm, focused in high performance and heavy loaded online newspapers.
+
+        For a long time his main focus was web performance <a href="https://github.com/frandieguez">engineering</a> and open source technology.
+
+        Prior to focusing on online newspapers, Fran worked as a open-source software developer at the University of Santiago de Compostela, where he have <strong>developed a custom Linux-based desktop</strong> for simplifying administrative tasks and also doing massive migrations to open source sofware in educational settings. This project <a href="http://www.mancomun.org/es/no_cache/actualidade/detalledenova/nova/a-osl-da-usc-gana-o-premio-eganet-2009-na-categoria-software-libre/">won the Libre Software contest</a> in Galicia, Spain.
+
+        Whenever not thinking web development, or newspapers, Fran can be found contributing to <a href="https://www.ohloh.net/accounts/frandieguez">open-source projects</a> like <strong>GNOME </strong>(as a project member) or Ubuntu as Galician translators coordinator, <a href="https://www.amazon.com/gp/pdp/profile/A362F32UBZHEUR">reading</a> or doing sports.
+
+        Currently is <strong>not available for hire</strong> but you can write him to propose future and innovative projects.
+
+        <h1>Not Found</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Layout>
+    )
+  }
+}
+
+export default AboutPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
