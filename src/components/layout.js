bc7c01db9ddd2406d1c9bbd6fbfd4c37e2669bdd
Fran Dieguez <fran.dieguez@mabishu.com>
2019-02-22 19:14:52 +0100

    Add top menu in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,74 +1,98 @@
 import React from "react"
-import { Link } from "gatsby"
+import { StaticQuery, graphql, Link } from "gatsby"
+import Image from "gatsby-image"
 
-import { rhythm, scale } from "../utils/typography"
+import { rhythm } from "../utils/typography"
+import layoutStyles from "../styles/layout.module.scss"
 
 class Layout extends React.Component {
   render() {
     const { location, title, children } = this.props
     const rootPath = `${__PATH_PREFIX__}/`
     let header
+    let HeaderContent = (
+      <StaticQuery
+        query={SiteQuery}
+        render={data => {
+          return (
+            <Link to={`/`}>
+              <Image
+                fixed={data.siteLogo.childImageSharp.fixed}
+                alt={title}
+              />
+            </Link>
+          )
+        }
+      }
+      />
+    )
 
     if (location.pathname === rootPath) {
       header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
+        <h1 className={layoutStyles.siteTitle}>
+          {HeaderContent}
         </h1>
       )
     } else {
       header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
+        <h3 className={layoutStyles.siteTitle}>
+          {HeaderContent}
         </h3>
       )
     }
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
-        <header>{header}</header>
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Web app designed and coded
-          {` `} using <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+      <div class="page-wrapper">
+
+        <header className={layoutStyles.Header}>
+
+          {header}
+
+          <section className={layoutStyles.Menu}>
+            <ul>
+              <li>
+                <Link to={'/about'}>The author</Link>
+              </li>
+              <li>
+                <Link to={'/'}>Thoughts</Link>
+              </li>
+              <li>
+                <Link to={'/archives'}>About</Link>
+              </li>
+              <li>
+                <Link to={'/contact'}>Contact</Link>
+              </li>
+            </ul>
+          </section>
+        </header>
+
+        <div
+          style={{
+            marginLeft: `auto`,
+            marginRight: `auto`,
+            maxWidth: rhythm(24),
+            padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          }}
+          >
+          <main>{children}</main>
+          <footer className={layoutStyles.Footer}>
+            © {new Date().getFullYear()}, Web app designed and coded
+            by <strong>Fran Dieguez</strong> using <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
       </div>
     )
   }
 }
 
+const SiteQuery = graphql`
+  query StaticQuery {
+    siteLogo: file(absolutePath: { regex: "/logos/glasses.png/" }) {
+      childImageSharp {
+        fixed(width: 375, height: 71) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
 export default Layout
