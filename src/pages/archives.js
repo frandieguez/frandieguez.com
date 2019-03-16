867ab5716a7feef44b09455fb4b9ae8201c3ad1a
Fran Dieguez <fran@openhost.es>
2019-03-16 19:24:09 +0100

    Add archives page

---
+++ src/pages/archives.js
@@ -0,0 +1,77 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+
+// import Bio from "../components/bio"
+import Layout from "../components/layout"
+import Wip from "../components/wip"
+import SEO from "../components/seo"
+// import Post from "../components/Post"
+
+import indexStyles from "../styles/archive.module.scss"
+
+class BlogIndex extends React.Component {
+  render() {
+    const { data }  = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts     = data.allMarkdownRemark.edges
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title="All posts"
+          keywords={data.site.siteMetadata.keywords}
+        />
+        {/* <Bio /> */}
+
+        <h3>Recently Published</h3>
+        <ul>
+        {posts.map(({ node }) => {
+          let title = node.frontmatter.title || node.fields.slug
+          return (
+            <li key={node.fields.slug} className={indexStyles.post}>
+                <Link to={node.fields.slug}>
+                  {title}
+                </Link>
+            </li>
+          )
+        })}
+        </ul>
+        <h3>Archives by Month</h3>
+        <div>In construction</div>
+        <p></p>
+
+        <h3>Archives by year</h3>
+        <div>In construction</div>
+      </Layout>
+    )
+  }
+}
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title,
+        keywords
+      }
+    }
+    allMarkdownRemark(
+      filter: { frontmatter: { published: {eq: true }} },
+      sort: { fields: [frontmatter___date], order: DESC },
+      limit: 30) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
+export default BlogIndex
