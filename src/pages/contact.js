108cb3b79b7766caad853c06ede5cada53459fd3
Fran Dieguez <fran.dieguez@mabishu.com>
2019-03-20 00:05:15 +0100

    Add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+class AboutPage extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="Contact" />
+
+        <p>
+          If you have anything to say to me. Use the form below. 👇
+        </p>
+
+        <form name="contact" netlify>
+          <p>
+            <label>Name <input type="text" name="name" /></label>
+          </p>
+          <p>
+            <label>Email <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+
+      </Layout>
+    )
+  }
+}
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
+
+export default AboutPage
