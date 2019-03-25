752edf41896455c6ac12edb64b458bfa5585ce0f
Fran Dieguez <fran@openhost.es>
2019-03-26 00:21:23 +0100

    Only show pages that are published

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,6 +9,7 @@ exports.createPages = ({ graphql, actions }) => {
     `
       {
         allMarkdownRemark(
+          filter: { frontmatter: { published: {eq: true }} },
           sort: { fields: [frontmatter___date], order: DESC }
           limit: 1000
         ) {
