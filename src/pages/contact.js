7f362b37cdcf3db94724f72fa1ed04790712c614
Fran Dieguez <fran.dieguez@mabishu.com>
2019-04-16 02:04:27 +0200

    Center contents in contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,24 +13,27 @@ class AboutPage extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="Contact" />
 
-        <p>
-          If you have anything to say to me. Use the form below. <span role='img' aria-label='down'>👇</span>
-        </p>
 
-        <form name="contact" className="contact" netlify>
+        <div className="aligncenter">
           <p>
-            <input type="text" placeholder="Your name" name="name" required/>
+            If you have anything to say to me. Use the form below. <span role='img' aria-label='down'>👇</span>
           </p>
-          <p>
-            <input type="email" placeholder="Your email" name="email" />
-          </p>
-          <p>
-            <textarea placeholder="Your message" name="message" id="message" cols="30" rows="10" required></textarea>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+
+          <form name="contact" className="contact" netlify>
+            <p>
+              <input type="text" placeholder="Your name" name="name" required/>
+            </p>
+            <p>
+              <input type="email" placeholder="Your email" name="email" />
+            </p>
+            <p>
+              <textarea placeholder="Your message" name="message" id="message" cols="30" rows="10" required></textarea>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+        </div>
 
       </Layout>
     )
