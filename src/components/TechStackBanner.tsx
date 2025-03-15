5f6b8881896c2f1463628fb902d0bcab737efd94
Fran Dieguez <fran.dieguez@situm.com>
2025-03-15 17:04:37 +0100

    Improve TechStackBanner

--- src/components/TechStackBanner.tsx
+++ src/components/TechStackBanner.tsx
@@ -1,29 +1,44 @@
-import React from "react";
-import { FaJava, FaReact, FaNodeJs } from "react-icons/fa";
-import { SiTypescript, SiRedux, SiMaplibre, SiWebgl } from "react-icons/si";
+import React, { useEffect, useRef } from "react";
+import { FaJava, FaReact, FaNodeJs, FaGit } from "react-icons/fa";
+import { SiTypescript, SiRedux, SiMaplibre, SiWebgl, SiFigma, SiPostgresql, SiSpringboot, SiGraphql, SiTailwindcss } from "react-icons/si";
 import { CiGlobe } from 'react-icons/ci';
 import { DiRuby } from "react-icons/di";
+import { motion, useAnimation } from "framer-motion";
 
 const technologies = [
-  { name: "Java", icon: <FaJava className="text-gray-700 text-4xl" />, text: "Java", link: "https://www.java.com/" },
-  { name: "Ruby", icon: <DiRuby className="text-gray-700 text-4xl" />, text: "Ruby", link: "https://www.ruby-lang.org/" },
-  { name: "TypeScript", icon: <SiTypescript className="text-gray-700 text-4xl" />, text: "TypeScript", link: "https://www.typescriptlang.org/" },
-  { name: "Node.js", icon: <FaNodeJs className="text-gray-700 text-4xl" />, text: "Node.js", link: "https://nodejs.org/" },
-  { name: "React", icon: <FaReact className="text-gray-700 text-4xl" />, text: "React", link: "https://react.dev/" },
-  { name: "Redux", icon: <SiRedux className="text-gray-700 text-4xl" />, text: "Redux", link: "https://redux.js.org/" },
-  { name: "MapLibre", icon: <SiMaplibre className="text-gray-700 text-4xl" />, text: "MapLibre", link: "https://maplibre.org/" },
-  { name: "Deck.gl", icon: <CiGlobe className="text-gray-700 text-4xl" />, text: "Deck.gl", link: "https://deck.gl/" },
-  { name: "WebGL", icon: <SiWebgl className="text-gray-700 text-4xl" />, text: "WebGL", link: "https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API" }
+  { name: "Java", icon: <FaJava />, link: "https://www.java.com/" },
+  // { name: "Spring Boot", icon: <SiSpringboot />, link: "https://spring.io/projects/spring-boot" },
+  // { name: "PostgreSQL", icon: <SiPostgresql />, link: "https://www.postgresql.org/" },
+  // { name: "GraphQL", icon: <SiGraphql />, link: "https://graphql.org/" },
+  { name: "Ruby", icon: <DiRuby />, link: "https://www.ruby-lang.org/" },
+  { name: "TypeScript", icon: <SiTypescript />, link: "https://www.typescriptlang.org/" },
+  { name: "Node.js", icon: <FaNodeJs />, link: "https://nodejs.org/" },
+  { name: "React", icon: <FaReact />, link: "https://react.dev/" },
+  { name: "Redux", icon: <SiRedux />, link: "https://redux.js.org/" },
+  { name: "MapLibre", icon: <SiMaplibre />, link: "https://maplibre.org/" },
+  { name: "Deck.gl", icon: <CiGlobe />, link: "https://deck.gl/" },
+  { name: "WebGL", icon: <SiWebgl />, link: "https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API" },
+  // { name: "Tailwind CSS", icon: <SiTailwindcss />, link: "https://tailwindcss.com/" },
+  // { name: "Figma", icon: <SiFigma />, link: "https://www.figma.com/" },
+  // { name: "Git", icon: <FaGit />, link: "https://git-scm.com/" }
 ];
+
 
 const TechStackBanner = () => {
   return (
-    <div className="w-full py-4 flex justify-center">
-      <div className="flex space-x-6">
+    <div className="w-full py-4">
+      <div className="flex flex-wrap gap-6 justify-center md:flex-nowrap">
         {technologies.map((tech, index) => (
-          <a key={index} href={tech.link} target="_blank" rel="noopener noreferrer" className="flex flex-col items-center hover:text-blue-500">
-            {tech.icon}
-            <span className="text-gray-700 text-sm mt-2">{tech.text}</span>
+          <a
+            key={index}
+            href={tech.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex flex-col items-center hover:text-blue-500"
+            title={`Language I use: ${tech.name}`}
+          >
+            <div className="text-gray-700 dark:text-gray-300 text-4xl">{tech.icon}</div>
+            <span className="text-gray-700 dark:text-gray-300 text-sm mt-2 whitespace-nowrap">{tech.name}</span>
           </a>
         ))}
       </div>
